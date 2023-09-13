3e656e163c554a77cf338116c7c35e92557a0b5e
cshei <cyshei@gmail.com>
2023-09-13 04:47:05 -0700

    Update fix-symlinks.sh to add symlinks in /usr/local/bin instead of /usr/bin

--- config/scripts/fix-symlinks.sh
+++ config/scripts/fix-symlinks.sh
@@ -5,5 +5,5 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-echo 'L /usr/bin/go - - - - /usr/lib/golang/bin/go' > /usr/lib/tmpfiles.d/golang.conf
-echo 'L /usr/bin/ld - - - - /usr/bin/ld.mold' > /usr/lib/tmpfiles.d/mold.conf
+echo 'L /usr/local/bin/go - - - - /usr/lib/golang/bin/go' > /usr/lib/tmpfiles.d/golang.conf
+echo 'L /usr/local/bin/ld - - - - /usr/bin/ld.mold' > /usr/lib/tmpfiles.d/mold.conf
